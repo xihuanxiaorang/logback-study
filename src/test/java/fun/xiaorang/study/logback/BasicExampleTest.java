a7972222eed561cd4be48815115b5a15a0e74284
小让 <15019474951@163.com>
2024-04-26 17:44:41 +0800

    feat: 演示记录器上下文的使用

--- src/test/java/fun/xiaorang/study/logback/BasicExampleTest.java
+++ src/test/java/fun/xiaorang/study/logback/BasicExampleTest.java
@@ -1,5 +1,6 @@
 package fun.xiaorang.study.logback;
 
+import org.junit.jupiter.api.Test;
 import org.slf4j.LoggerFactory;
 
 /**
@@ -7,12 +8,13 @@ import org.slf4j.LoggerFactory;
  * @description <p style = " font-weight:bold ; "><p/>
  * @github <a href="https://github.com/xihuanxiaorang/logback-study">logback-study</a>
  * @Copyright 博客：<a href="https://docs.xiaorang.fun">小让的糖果屋</a>  - show me the code
- * @date 2024/04/26 16:45
+ * @date 2024/04/26 17:42
  */
-public class Example {
-  private static final ch.qos.logback.classic.Logger LOGGER = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger(Example.class);
+public class BasicExampleTest {
+  private static final ch.qos.logback.classic.Logger LOGGER = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger(BasicExampleTest.class);
 
-  public static void main(String[] args) {
-    LOGGER.info("Example log from {}", Example.class.getSimpleName());
+  @Test
+  public void test() {
+    LOGGER.info("Example log from {}", BasicExampleTest.class.getSimpleName());
   }
 }
---
+++ src/test/java/fun/xiaorang/study/logback/LoggerContextTest.java
@@ -0,0 +1,39 @@
+package fun.xiaorang.study.logback;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author xiaorang
+ * @description <p style = " font-weight:bold ; "><p/>
+ * @github <a href="https://github.com/xihuanxiaorang/logback-study">logback-study</a>
+ * @Copyright 博客：<a href="https://docs.xiaorang.fun">小让的糖果屋</a>  - show me the code
+ * @date 2024/04/26 16:53
+ */
+public class LoggerContextTest {
+  @Test
+  public void test() {
+    ch.qos.logback.classic.Logger parentLogger =
+            (ch.qos.logback.classic.Logger) LoggerFactory.getLogger("fun.xiaorang.study.logback");
+    parentLogger.setLevel(ch.qos.logback.classic.Level.INFO);
+    ch.qos.logback.classic.Logger childlogger =
+            (ch.qos.logback.classic.Logger) LoggerFactory.getLogger("fun.xiaorang.study.logback.tests");
+    parentLogger.warn("This message is logged because WARN > INFO.");
+    parentLogger.debug("This message is not logged because DEBUG < INFO.");
+    childlogger.info("INFO == INFO");
+    childlogger.debug("DEBUG < INFO");
+  }
+
+  @Test
+  public void test1() {
+    ch.qos.logback.classic.Logger logger =
+            (ch.qos.logback.classic.Logger) LoggerFactory.getLogger("fun.xiaorang.study.logback");
+    logger.debug("Hi there!");
+    logger.debug("This message is logged because DEBUG == DEBUG.");
+    ch.qos.logback.classic.Logger rootLogger =
+            (ch.qos.logback.classic.Logger) LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
+    rootLogger.setLevel(ch.qos.logback.classic.Level.ERROR);
+    logger.warn("This message is not logged because WARN < ERROR.");
+    logger.error("This is logged.");
+  }
+}
